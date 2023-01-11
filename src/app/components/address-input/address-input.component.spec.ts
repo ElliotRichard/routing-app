9733574c2ae9e46ee8cb7e57cd255970d770be9d
RichardElliot <elliotrichard@protonmail.com>
2023-01-12 02:17:21 +1300

    address-input component unit testing

--- src/app/components/address-input/address-input.component.spec.ts
+++ src/app/components/address-input/address-input.component.spec.ts
@@ -1,25 +1,47 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { AddressInputComponent } from './address-input.component';
-
-describe('AutocompleteComponent', () => {
+import { RouteStringPipe } from '../../pipes/route-string.pipe';
 import { ROUTE } from 'src/shared/types';
-  let component: AddressInputComponent;
-  let fixture: ComponentFixture<AddressInputComponent>;
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [AddressInputComponent],
-    }).compileComponents();
-  });
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(AddressInputComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+describe('AddressInputComponent', () => {
+	let component: AddressInputComponent;
+	let fixture: ComponentFixture<AddressInputComponent>;
+    
+    beforeEach(async () => {
+	await TestBed.configureTestingModule({
+	    declarations: [
+			AddressInputComponent,
+			RouteStringPipe,
+	    ],
+	    imports: [
+			HttpClientTestingModule,
+			MatFormFieldModule,
+			MatInputModule,
+			NoopAnimationsModule,
+	    ],
+	    providers: [
+		{
+	        provide: MAT_FORM_FIELD_DEFAULT_OPTIONS,
+		    useValue: { floatLabel: 'always' },
+		},
+	    ]
+	}).compileComponents();
+	fixture = TestBed.createComponent(AddressInputComponent);
+	component = fixture.componentInstance;
+	component.type = ROUTE.START;
+	fixture.detectChanges();
+    });
+    
+    it('should create', () => {
+		expect(component).toBeTruthy();
+    });
+    
+    it('should be the the first input of the page', () => {
+		expect(component.type).toBe(ROUTE.START);
+    });
 });
