577814ba80475290d871e71ace5bf0377459327a
RichardElliot <elliotrichard@protonmail.com>
2023-01-12 03:17:53 +1300

    scaffolded unit tests for sign-in.component.ts

--- src/app/components/user/sign-in/sign-in.component.spec.ts
+++ src/app/components/user/sign-in/sign-in.component.spec.ts
@@ -1,14 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { FireBaseService } from '../../../services/firebase.service';
 import { SignInComponent } from './sign-in.component';
+import { AngularFireModule, FIREBASE_OPTIONS } from '@angular/fire/compat';
+import { environment } from '../../../../environments/environment';
 
 describe('SignInComponent', () => {
   let component: SignInComponent;
   let fixture: ComponentFixture<SignInComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [SignInComponent],
+    beforeEach(async () => {
+	await TestBed.configureTestingModule({
+	    declarations: [SignInComponent],
+	    providers: [
+		FireBaseService,
+		{ provide: FIREBASE_OPTIONS, useValue: environment.firebase },
+	],
     }).compileComponents();
   });
 
