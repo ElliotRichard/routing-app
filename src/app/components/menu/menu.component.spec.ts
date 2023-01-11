0c97e4237e527742e9233a7662b9e84b5d99e9af
RichardElliot <elliotrichard@protonmail.com>
2023-01-12 02:34:49 +1300

    scaffolded unit tests for menu components

--- src/app/components/menu/menu.component.spec.ts
+++ src/app/components/menu/menu.component.spec.ts
@@ -1,14 +1,21 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { fakeFireBaseService } from '../user/user-dialog/user-dialog.component.spec';
+import { FireBaseService } from '../../services/firebase.service';
 import { MenuComponent } from './menu.component';
+import { FIREBASE_OPTIONS } from '@angular/fire/compat';
+import { environment } from '../../../environments/environment';
 
 describe('MenuComponent', () => {
-  let component: MenuComponent;
-  let fixture: ComponentFixture<MenuComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ MenuComponent ]
+    let component: MenuComponent;
+    let fixture: ComponentFixture<MenuComponent>;
+    
+    beforeEach(async () => {
+      await TestBed.configureTestingModule({
+	declarations: [ MenuComponent ],
+	providers: [
+	    FireBaseService,
+	    { provide: FIREBASE_OPTIONS, useValue: environment.firebase },
+	],
     })
     .compileComponents();
   });
