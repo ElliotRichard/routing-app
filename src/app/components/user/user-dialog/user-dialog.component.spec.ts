30b78c788cb5eaa41d5f65b7b03ab9f4678055d9
RichardElliot <elliotrichard@protonmail.com>
2023-01-12 03:21:25 +1300

    added a firebase user collection mock to user-dialog.component unit tests

--- src/app/components/user/user-dialog/user-dialog.component.spec.ts
+++ src/app/components/user/user-dialog/user-dialog.component.spec.ts
@@ -1,14 +1,50 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { MatDialogModule } from '@angular/material/dialog';
 import { UserDialogComponent } from './user-dialog.component';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { FormBuilder } from '@angular/forms';
+import { FireBaseService } from '../../../services/firebase.service';
+import { FIREBASE_OPTIONS } from '@angular/fire/compat';
+import { environment } from '../../../../environments/environment';
+import { Observable, of, Subject } from 'rxjs';
+
+export const mockUserCollection = [
+    {name: "mocked_dog",  address: "123 test st", owner: "mocked_owner", notes: ""},
+    {name: "mocked_puppy", address: "456 test rd",owner: "mocked_owner_2", notes: "be polite" },
+
+];
+
+export const fakeFireBaseService:
+  Pick<FireBaseService, keyof FireBaseService> = {
+      getUserCollection(): Observable<any> {
+	  return of(mockUserCollection);
+  },
+      authenticationStatus: of(true) as Subject<boolean>,
+      signInUser() {},
+      signOutUser() {},
+      addDog() {},
+      deleteDog() {}
+};
+
 
 describe('UserDialogComponent', () => {
+  // An anonymous user
+  // const authState: MockUser
   let component: UserDialogComponent;
   let fixture: ComponentFixture<UserDialogComponent>;
+  let mockFireBaseService = FireBaseService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ UserDialogComponent ]
+      imports: [ MatDialogModule],
+      declarations: [ UserDialogComponent ],
+	providers: [
+	  { provide: FireBaseService, useValue: fakeFireBaseService },
+      FormBuilder,
+	  { provide: FIREBASE_OPTIONS, useValue: environment.firebase },
+      { provide: MatDialogRef, useValue: {} },
+      { provide: MAT_DIALOG_DATA, useValue: {} }
+    ]
     })
     .compileComponents();
   });
