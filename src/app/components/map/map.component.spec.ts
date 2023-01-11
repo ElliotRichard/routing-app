6cfafae5d334c202751d1ccffbe2c70d57719775
RichardElliot <elliotrichard@protonmail.com>
2023-01-12 02:34:14 +1300

    scaffolded unit tests for the map

--- src/app/components/map/map.component.spec.ts
+++ src/app/components/map/map.component.spec.ts
@@ -1,14 +1,21 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { GoogleMapsModule } from '@angular/google-maps';
 
 import { MapComponent } from './map.component';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import {HttpClientModule} from '@angular/common/http';
 
-describe('AlternateMapComponent', () => {
+describe('MapComponent', () => {
   let component: MapComponent;
   let fixture: ComponentFixture<MapComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [MapComponent],
+	imports: [
+	    GoogleMapsModule,
+	    HttpClientTestingModule
+	],
     }).compileComponents();
   });
 
