55f1c99cc10f0c0e5ec0c840ecc626f4c4cf3f5b
RichardElliot <elliotrichard@protonmail.com>
2023-01-12 02:45:14 +1300

    scaffolded unit tests for firebase.service.ts

--- src/app/services/firebase.service.spec.ts
+++ src/app/services/firebase.service.spec.ts
@@ -1,14 +1,22 @@
 import { TestBed } from '@angular/core/testing';
-
 import { FireBaseService } from './firebase.service';
+import { AngularFireModule } from '@angular/fire/compat';
+import { FIREBASE_OPTIONS } from '@angular/fire/compat';
+import { environment } from '../../environments/environment';
 
 describe('FirebaseService', () => {
-  let service: FireBaseService;
-
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(FireBaseService);
-  });
+    let service: FireBaseService;
+    
+    beforeEach(() => {
+	TestBed.configureTestingModule({
+	    imports: [
+			AngularFireModule.initializeApp(environment.firebase),
+	    ], 
+	    providers: [
+			{ provide: FIREBASE_OPTIONS, useValue: environment.firebase }
+	    ]});
+	service = TestBed.inject(FireBaseService);
+    });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
